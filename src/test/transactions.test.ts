138fe5b070d376efbd2bfc39525665029a65ad4c
Nvutu-Eladio <Eladio2019@hotmail.com>
2023-06-28 17:44:11 -0300

    categorizando os testes

---
+++ src/test/transactions.test.ts
@@ -0,0 +1,25 @@
+import { test, beforeAll, afterAll, describe} from 'vitest'
+import request from 'supertest'
+import { app } from '../app'
+
+describe('Transactions routes', ()=>{
+    beforeAll(async() =>{
+        await app.ready()
+    }) 
+    
+    afterAll( async () =>{
+        await app.close()
+    })
+    
+    test('O usuário consegue criar uma nova transação', async () =>{
+        await request(app.server)
+        .post('/transactions')
+        .send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit'
+        })
+        .expect(201)
+    })
+})
+
