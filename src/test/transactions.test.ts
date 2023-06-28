6011691ab3e39ac08cd2dcc9ba43ccb6ae58f8eb
Nvutu-Eladio <Eladio2019@hotmail.com>
2023-06-28 18:08:17 -0300

    Testando listagem de transacoes

--- src/test/transactions.test.ts
+++ src/test/transactions.test.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe} from 'vitest'
+import { test, beforeAll, afterAll, describe, expect} from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
 
@@ -21,5 +21,31 @@ describe('Transactions routes', ()=>{
         })
         .expect(201)
     })
+
+    test('O usuário consegue listar as transações', async()=>{
+        const createTransactionResponse = await request(app.server)
+        .post('/transactions')
+        .send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit'
+        })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+       const listTransactionResponse = await request(app.server)
+        .get('/transactions')
+        .set('Cookie', cookies)
+        .expect(200)
+
+       expect(listTransactionResponse.body.transactions).toEqual([
+        expect.objectContaining({
+            title: 'New transaction',
+            amount: 5000,
+        })
+
+       ])
+    })
+
 })
 
