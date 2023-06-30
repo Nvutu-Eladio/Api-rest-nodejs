59e4ec3dfed3d90a56be5e5025cc25a8aad65659
Nvutu-Eladio <Eladio2019@hotmail.com>
2023-06-30 15:48:08 -0300

    refresh

--- src/database.ts
+++ src/database.ts
@@ -5,11 +5,11 @@ import { env } from './env';
 export const config: Knex.Config = {
     client: 'env.DATABASE_CLIENT',
     connection: 
-        env.DATABASE_CLIENT === 'sqlite'
+        process.env.DATABASE_CLIENT === 'sqlite'
         ? {
             filename: env.DATABASE_URL,
           }
-        : env.DATABASE_URL,
+        : process.env.DATABASE_URL,
     useNullAsDefault: true,
     migrations:{
         extension: 'ts',
